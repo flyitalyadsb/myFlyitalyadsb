95e73757e3919ad635bbd3cdf21a3b561f393788
flyitalyadsb <admin@flyitalyadsb.com>
2023-09-30 18:04:16 +0200

    catch clients.py error

--- modules/clients/clients.py
+++ modules/clients/clients.py
@@ -70,51 +70,54 @@ async def clients(session):
     ric_dict = {ric.uuid: ric for ric in ric_query.scalars().all()}
 
     async def add_or_update_receiver(receiver_readsb, client_readsb):
-        uuid = client_readsb[0]
-        ric: Receiver = ric_dict.get(uuid)
+        try:
+            uuid = client_readsb[0]
+            ric: Receiver = ric_dict.get(uuid)
 
-        # Base data
-        base_data = {
-            'last_seen': datetime.datetime.now(),
-            'uuid': uuid,
-            'position_counter': receiver_readsb[1],
-            'timed_out_counter': receiver_readsb[2],
-            'lat_min': receiver_readsb[3],
-            'lat_max': receiver_readsb[4],
-            'lon_min': receiver_readsb[5],
-            'lon_max': receiver_readsb[6],
-            'lat_avg': receiver_readsb[8],
-            'lon_avg': receiver_readsb[9],
-            'messagges_per_sec': client_readsb[4],
-            'ip': re.search(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", client_readsb[1]).group(0)
-        }
+            # Base data
+            base_data = {
+                'last_seen': datetime.datetime.now(),
+                'uuid': uuid,
+                'position_counter': receiver_readsb[1],
+                'timed_out_counter': receiver_readsb[2],
+                'lat_min': receiver_readsb[3],
+                'lat_max': receiver_readsb[4],
+                'lon_min': receiver_readsb[5],
+                'lon_max': receiver_readsb[6],
+                'lat_avg': receiver_readsb[8],
+                'lon_avg': receiver_readsb[9],
+                'messagges_per_sec': client_readsb[4],
+                'ip': re.search(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", client_readsb[1]).group(0)
+            }
 
-        # Update if receiver exists
-        if ric:
-            for key, value in base_data.items():
-                setattr(ric, key, value)
-        else:
-            ric = Receiver(**base_data)
-            session.add(ric)
+            # Update if receiver exists
+            if ric:
+                for key, value in base_data.items():
+                    setattr(ric, key, value)
+            else:
+                ric = Receiver(**base_data)
+                session.add(ric)
 
-        # Additional data from mlat clients
-        client_mlat_data = next(
-            (c for c in clients_mlat.values() if c["uuid"] and (c["uuid"] == uuid or uuid in c["uuid"])), None)
+            # Additional data from mlat clients
+            client_mlat_data = next(
+                (c for c in clients_mlat.values() if c["uuid"] and (c["uuid"] == uuid or uuid in c["uuid"])), None)
 
-        if client_mlat_data:
-            ric.lat = client_mlat_data["lat"]
-            ric.lon = client_mlat_data["lon"]
-            ric.alt = client_mlat_data["alt"]
-            ric.name = client_mlat_data["user"]
+            if client_mlat_data:
+                ric.lat = client_mlat_data["lat"]
+                ric.lon = client_mlat_data["lon"]
+                ric.alt = client_mlat_data["alt"]
+                ric.name = client_mlat_data["user"]
 
-            # Peers processing
-            peer_names = list(sync_mlat[client_mlat_data["user"]]["peers"].keys())
-            peer_query = await session.execute(select(Receiver).filter(Receiver.name.in_(peer_names)))
-            peer_dict = {peer.name: peer for peer in peer_query.scalars().all()}
-            peers = [peer_dict.get(name) for name in peer_names if
-                     peer_dict.get(name) and peer_dict.get(name) not in ric.peers]
-            if peers:
-                ric.peers.extend(peers)
+                # Peers processing
+                peer_names = list(sync_mlat[client_mlat_data["user"]]["peers"].keys())
+                peer_query = await session.execute(select(Receiver).filter(Receiver.name.in_(peer_names)))
+                peer_dict = {peer.name: peer for peer in peer_query.scalars().all()}
+                peers = [peer_dict.get(name) for name in peer_names if
+                         peer_dict.get(name) and peer_dict.get(name) not in ric.peers]
+                if peers:
+                    ric.peers.extend(peers)
+        except Exception as e:
+            print(f"Receiver: {client_readsb[0]} not elaborated, error: {e}")
 
     # Processing
     for receiver_readsb in tqdm(receivers_readsb, desc="Adding CLIENTS to internal db"):
